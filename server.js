f1f95cfc9cee99a730ebc5946491070a68c956e5
Glitch (roblox-ve) <none>
2020-10-12 03:35:57 +0000

    verify

--- server.js
+++ server.js
@@ -1,85 +1,68 @@
+require("http").createServer(async (req,res) => { res.statusCode = 200; res.write("ok"); res.end(); }).listen(3000, () => console.log("Now listening on port 3000"));
+
 const Discord = require("discord.js");
-const fs = require("fs");
-const client = new Discord.Client();
-const { Prefix, Token, Color } = require("./config.js");
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
-client.db = require("quick.db");
+const { Client, Collection } = require("discord.js");
+const { config } = require("dotenv");
 
-client.on("ready", async () => {
-  console.log(`ready!`);
-  client.user
-    .setActivity(`MADE BY MYBOT`)
-    .catch(error => console.log(error));
+const client = new Client({
+    disableEveryone: true
+})
+
+
+// Collections
+client.commands = new Collection();
+client.aliases = new Collection();
+
+config({
+    path: __dirname + "/.env"
+});
+
+// Run the command loader
+["command"].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
 });
 
 client.on("message", async message => {
-  if (message.channel.type === "dm") return;
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.member)
-    message.member = await message.guild.fetchMember(message);
+    const prefix = "!";
 
-  if (message.content.match(new RegExp(`^<@!?${client.user.id}>`))) {
-    return message.channel.send(`Bot Prefix : ${Prefix}`);
-  }
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // If message.member is uncached, cache it.
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    
+    if (cmd.length === 0) return;
+    
+    // Get the command
+    let command = client.commands.get(cmd);
+    // If none is found, try to find it by alias
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    // If a command is finally found, run the command
+    if (command) 
+        command.run(client, message, args);
 });
 
-let modules = ["fun", "info", "moderation"];
+// WELCOME MESSAGe
 
-modules.forEach(function(module) {
-  fs.readdir(`./commands/${module}`, function(err, files) {
-    if (err)
-      return new Error(
-        "Missing Folder Of Commands! Example : Commands/<Folder>/<Command>.js"
-      );
-    files.forEach(function(file) {
-      if (!file.endsWith(".js")) return;
-      let command = require(`./commands/${module}/${file}`);
-      console.log(`${command.name} Command Has Been Loaded - ✅`);
-      if (command.name) client.commands.set(command.name, command);
-      if (command.aliases) {
-        command.aliases.forEach(alias =>
-          client.aliases.set(alias, command.name)
-        );
-      }
-      if (command.aliases.length === 0) command.aliases = null;
-    });
-  });
+ client.on("guildMemberAdd", (member) => {
+
+    let channel = client.channels.get('627712783957491712');
+
+    const embed = new Discord.RichEmbed()
+    .setColor("#2ecc71")
+    .setTitle(`**Welcome**`)
+    .setDescription(`**Welcome!** ${member} **to Vero! We hope you have a good time here!**`)
+    .setAuthor("VeroAPI","https://i.imgur.com/UaHfuUX.png")
+    .setTimestamp()
+    .setImage("https://i.imgur.com/UaHfuUX.png")
+    
+    channel.send(embed)
 });
 
-client.on("message", async message => {
-  if (message.channel.type === "dm") return;
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.member)
-    message.member = await message.guild.fetchMember(message);
 
-  if (!message.content.startsWith(Prefix)) return;
-
-  const args = message.content
-    .slice(Prefix.length)
-    .trim()
-    .split(" ");
-  const cmd = args.shift().toLowerCase();
-
-  if (cmd.length === 0) return;
-
-  let command =
-    client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
-
-  if (!command) return;
-
-  if (command) {
-    if (!message.guild.me.hasPermission("ADMINISTRATOR"))
-      return message.channel.send(
-        "I Don't Have Enough Permission To Use This Or Any Of My Commands | Require : Administrator"
-      );
-    command.run(client, message, args);
-  }
-  console.log(
-    `User : ${message.author.tag} (${message.author.id}) Server : ${message.guild.name} (${message.guild.id}) Command : ${command.name}`
-  );
-});
-
-client.login(Token);
+client.login(process.env.TOKEN);
